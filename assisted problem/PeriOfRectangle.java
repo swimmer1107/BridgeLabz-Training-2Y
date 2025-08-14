d08bd91e7754d7fc5beffdc44b6a503103c847d9
Pulkit Kulshreshtha <pulkitvip1107@gmail.com>
2025-08-14 22:19:26 +0530

    First comment

---
+++ assisted problem/PeriOfRectangle.java
@@ -0,0 +1,9 @@
+public class PeriOfRectangle {
+    public static void main(String[] args) {
+        double length = 10.0; // Example length
+        double width = 5.0; // Example width
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+    
+}
---
+++ assisted problem/VolumeofaCylinder.java
@@ -0,0 +1,9 @@
+public class VolumeofaCylinder {
+    public static void main(String[] args) {
+        double radius = 7.0; // Example radius
+        double height = 10.0; // Example height
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("Volume of the cylinder: " + volume);
+    }
+    
+}
